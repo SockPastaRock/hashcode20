4690232cfe080a0f67b7ee1a8a8127f090fcec1a
Chris Abram <cabram03@gmail.com>
2020-01-20 11:44:06 +0000

    gitignore fix.

--- practice/solutions/python/run.py
+++ practice/solutions/python/run.py
@@ -12,15 +12,36 @@ TESTS_DIR = "./tests/"
 # }}}
 # {{{ Functions
 
-# {{{ maxSlices
+# {{{ slicesCombination
 
 
-def maxSlices(n, m):
+def slicesCombination(tar, slices):
     """Docstring for max_slices"""
 
-    print()
-    print(n)
-    print(m)
+    slice_sums = [0]
+    combinations = [[]]
+
+    for ix_slice, slice in enumerate(slices):
+        tmp_sums = []
+        tmp_combs = []
+        for ix_sum, slice_sum in enumerate(slice_sums):
+            tmp_sums.append(slice_sum + slice)
+            tmp_combs.append(combinations[ix_sum] + [ix_slice])
+
+        slice_sums += tmp_sums
+        combinations += tmp_combs
+
+    min_diff = tar
+    min_diff_ix = 0
+    for ix, slice_sum in enumerate(slice_sums):
+        diff = tar - slice_sum
+        if diff == 0:
+            return combinations[ix]
+        if diff > 0:
+            if diff < min_diff:
+                min_diff = diff
+                min_diff_ix = ix
+    return combinations[min_diff_ix]
 
 # }}}
 
@@ -29,10 +50,13 @@ def maxSlices(n, m):
 if __name__ == '__main__':
     for test_case in os.listdir(TESTS_DIR):
         with open(TESTS_DIR + test_case, "r") as f:
-            n = list(map(int, f.readline().split(" ")))
-            m = list(map(int, f.readline().split(" ")))
+            target, num_pizzas = list(map(int, f.readline().split(" ")))
+            slices = list(map(int, f.readline().split(" ")))
 
-        result = maxSlices(n, m)
+        result = slicesCombination(target, slices)
+
+        print()
+        print(result)
 
 """
 
